9543dfb81aa7027784966f480eaf26955a8444c7
Lucas de Paula <lucas.depaula@smartnx.io>
2023-06-12 00:45:50 -0300

    first commit

---
+++ src/components/Underline.tsx
@@ -0,0 +1,19 @@
+export function Underline({ className }: { className?: string }) {
+  return (
+    <svg
+      className={className}
+      width="266"
+      height="22"
+      viewBox="0 0 266 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M62.5 2L265 4L0.5 11C61.6667 9.33333 195.8 8.8 243 20"
+        stroke="#8257E6"
+        stroke-width="3"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+}
